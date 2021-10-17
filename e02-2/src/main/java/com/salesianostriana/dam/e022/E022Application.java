75cfee2e7b30f582108178a3620cfd845d36b50d
efatuarte <fatuarte.feern21@triana.salesianos.edu>
2021-10-17 12:04:58 +0200

    Solucionado apartado 2 del ejercicio

---
+++ e02-2/src/main/java/com/salesianostriana/dam/e022/E022Application.java
@@ -0,0 +1,13 @@
+package com.salesianostriana.dam.e022;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class E022Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(E022Application.class, args);
+	}
+
+}
---
+++ e02-2/src/main/java/com/salesianostriana/dam/e022/Producto.java
@@ -0,0 +1,25 @@
+package com.salesianostriana.dam.e022;
+
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import org.hibernate.annotations.Table;
+
+import javax.persistence.*;
+
+@Entity
+@Data
+@NoArgsConstructor
+@Table(name = "product")
+public class Producto {
+    @Id
+    @GeneratedValue(strategy= GenerationType.IDENTITY)
+    private Integer id;
+
+    @Column(name = "name", nullable = false)
+    private String nombre;
+
+    @Column(name = "price", nullable = false)
+    private float precio;
+
+    private String imagen, descripcion;
+}
